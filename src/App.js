b53afc71a380d237619cdb4a0987b8be23c0afa9
Luciano Pardo <pardobio@gmail.com>
2022-09-01 18:47:19 -0300

    starting nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import bonvoy from './assets/img/300x91_mb-tm_white.png'
 
 function App() {
   return (
     <div className="App">
+      <nav className='nav'>
+        <div>
+          <button></button>
+          <div><a href='https://www.ritzcarlton.com/en'> RITZCARLTON.COM</a></div>
+          <div>
+            <div><a href='/'>{`ALL HOTELS & RESORTS`}</a></div>
+            <div><a href='/'>CONTACT US</a></div>
+            <div><a href='/'>SIGN IN</a></div>
+            <div><img src={bonvoy} alr='bonvoy'/></div>
+          </div>
+        </div>
+
+      </nav>
     </div>
   );
 }
